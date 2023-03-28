fa86049ebd5f84a8d85026d20b3edcfc3e35349b
Alex <baklan4ik1993@gmail.com>
2023-03-28 17:54:47 +0200

    image

--- frontend/screens/ProfileScreen.jsx
+++ frontend/screens/ProfileScreen.jsx
@@ -1,48 +1,62 @@
-import { StyleSheet, Text, View, Image} from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const ProfileScreen = () => {
-    const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState(null)
 
-    useEffect(() => {
-        const fetchUserInfo = async () => {
-          try {
-            const value = await (AsyncStorage.getItem('UserInfo'))
-            
-            if (value !== null) {
-              const user = await JSON.parse(value)
-              const config = {
-                headers: {
-                    'Content-type': 'application/json',
-                    Authorization: `Bearer ${user?.token}`
-                }
-              }
-              const { data } = await axios.get(
-                "http://127.0.0.1:8000/api/users/refresh/",
-                config
-              );
-              setUserInfo(data);
-              console.log(userInfo)
-            }
-          } catch (error) {
-            console.log(error);
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const value = await AsyncStorage.getItem('UserInfo')
+
+        if (value !== null) {
+          const user = await JSON.parse(value)
+          const config = {
+            headers: {
+              'Content-type': 'application/json',
+              Authorization: `Bearer ${user?.token}`,
+            },
           }
-        };
-        fetchUserInfo();
-      }, []);
+          const { data } = await axios.get(
+            'http://127.0.0.1:8000/api/users/refresh/',
+            config
+          )
+          setUserInfo(data)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUserInfo()
+  }, [])
+
   return (
-    <View>
-      <View>
-        <Image url="http://127.0.0.1:8000/media/avatars/def_avatar.png"/>
-        <Text>{userInfo?.profile.full_username}</Text>
-      </View>
+    <View style={{padding: 20}}>
+      {userInfo && (
+        <View style={{flexDirection: "row", alignItems: "center", gap: 5}}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: `http://127.0.0.1:8000${userInfo.profile.avatar }` }}
+          />
+          <Text style={styles.username}>{userInfo?.profile.full_username}</Text>
+        </View>
+      )}
     </View>
   )
 }
 
 export default ProfileScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 500,
+  },
+  username: {
+    fontWeight: 700, 
+    fontSize: 20,
+  }
+})
