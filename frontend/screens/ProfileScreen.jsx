52529ccfeb03de1e92cb82e0e0c0dbf86b773167
Alex <baklan4ik1993@gmail.com>
2023-03-31 19:59:07 +0200

    avatar upload

--- frontend/screens/ProfileScreen.jsx
+++ frontend/screens/ProfileScreen.jsx
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Level from '../components/Level'
-
+import * as ImagePicker from 'expo-image-picker';
 const ProfileScreen = ({navigation}) => {
   const [userInfo, setUserInfo] = useState(null)
   const [games, setGames] = useState([])
-  
+  const [avatarUri, setAvatarUri] = useState(null);
+
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
@@ -38,6 +39,48 @@ const ProfileScreen = ({navigation}) => {
     fetchUserInfo()
   }, [])
   
+  const handleAvatarPress = async () => {
+    // Request permission to access the media library
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!permissionResult.granted) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+  
+    // Launch the image picker and wait for the user to select an image
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (!pickerResult.cancelled) {
+      // If an image was selected, update the component state with the URI of the selected image
+      setAvatarUri(pickerResult.assets[0].uri);
+  
+      // Create a new FormData object and append the selected image to it
+      const data = new FormData();
+      data.append("avatar", {
+        name: "avatar.jpg",
+        type: "image/jpg",
+        uri: pickerResult.assets[0].uri
+      });
+  
+      // Create a configuration object with the necessary headers
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${userInfo?.token}`
+        }
+      }
+  
+      try {
+        // Send a PUT request to the server with the FormData object and the configuration object
+        const response = await axios.put('http://127.0.0.1:8000/api/users/update/', data, config);
+        // Update the user info with the data returned from the server
+        setUserInfo(response.data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+  
+  
 
   const handleLogout = async () => {
     try {
@@ -48,7 +91,6 @@ const ProfileScreen = ({navigation}) => {
     }
   };
   
-
   return (
     <View style={{padding: 20, position: "relative", flex: 1}}>
       <View style={{flex:1}}>
@@ -56,12 +98,18 @@ const ProfileScreen = ({navigation}) => {
         <>
         <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
         <View style={{flexDirection: "row", alignItems: "center", gap: 5}}>
-          <TouchableOpacity>
-            <Image
-              style={styles.avatar}
-              source={{ uri: `http://127.0.0.1:8000${userInfo.profile.avatar }` }}
-            />
-          </TouchableOpacity>
+        <TouchableOpacity onPress={handleAvatarPress}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: `http://127.0.0.1:8000${userInfo.profile.avatar }` }}
+          />
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={handleAvatarPress}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: avatarUri ? avatarUri : `http://127.0.0.1:8000${userInfo.profile.avatar }` }}
+          />
+        </TouchableOpacity> */}
           <Text style={styles.username}>{userInfo?.profile.full_username}</Text>
         </View>
           <View style={{backgroundColor: "#000", paddingHorizontal: 10, paddingVertical: 5, borderRadius: 5}}>
